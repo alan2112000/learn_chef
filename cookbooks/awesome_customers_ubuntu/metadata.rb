f4fb4b10626ba8d29e0370294496623f6463ea7d
Alan Yu <alan21120000@gmail.com>
2016-09-27 17:03:33 +0800

    Finish awesome customers default recipe

---
+++ cookbooks/awesome_customers_ubuntu/metadata.rb
@@ -0,0 +1,18 @@
+name 'awesome_customers_ubuntu'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures awesome_customers_ubuntu'
+long_description 'Installs/Configures awesome_customers_ubuntu'
+version '0.1.0'
+
+depends 'apt', '4.0.2'
+depends 'firewall'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/awesome_customers_ubuntu/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/awesome_customers_ubuntu' if respond_to?(:source_url)
